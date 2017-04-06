f34b9fc332e8096bc2a120930c980b1f4647e709
Posttest2 <hrtiniindriani@gmail.com>
2017-04-06 21:38:15 +0800

    posttest6

--- src/POSTTEST5/tugas1.java
+++ src/POSTTEST5/tugas1.java
@@ -210,7 +210,7 @@ private DefaultTableModel model;
             .addGroup(jPanel4Layout.createSequentialGroup()
                 .addGap(31, 31, 31)
                 .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(18, 18, 18)
                 .addComponent(jButton2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton3)
